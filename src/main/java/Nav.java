ee867d907e75bf1d30a2e42c01ef538fe326aefc
nick1 <n_jaku01@wwu.de>
2019-03-12 15:59:55 +0100

    Geocoding

--- src/main/java/Nav.java
+++ src/main/java/Nav.java
@@ -61,6 +61,10 @@ public class Nav {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+if (result==null)
+{
+
+}
 
                 return result;
             }
@@ -140,6 +144,13 @@ public class Nav {
 
     }
     });
+    try {
+
+        //sleep 5 seconds
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
     return currentRoute;
     }
 }
