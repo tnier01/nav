16afab6490dcee4fc936593658255358d9192028
Phil <p_huef01@uni-muenster.de>
2019-03-13 08:42:05 +0100

    Tests

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,22 @@
 public class Test {
 
+    /*Methode 1: getRoute(Point origin, Point destination, String profile): void
+        mögliche Fehlerquellen:
+            falsche Eingabe:
+                Eingabe ist kein Point
+                Koordinate auf Meer?
+
+     Methode 2: geocoder(String Eingabe): Point
+            falsche Eingabe:
+                keine Stadt
+
+     Methode 3: changeProfile (String profile): void
+            falsche Eingabe:
+
+     Methhode 4: wegbeschreibung(directionsRoute currentRoute)
+            falsche Eingabe:
+
+*/
 
 }
 
