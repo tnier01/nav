e3b52945a40170ca0c41d2cb455511ee33439d7a
Phil <p_huef01@uni-muenster.de>
2019-03-13 15:12:20 +0100

    Tests

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -10,43 +10,55 @@ public class Test {
 
         Routenfinder testNav = new Routenfinder();
         /*class Routenfinder
-        method 1: getRoute(String origin, String destination, String profile): DirectionsRoute
 
+        method 1: getRoute(String origin, String destination, String profile): DirectionsRoute
          possibilities:
              input is a not existing location -> IOException */
-            // assertEquals(testNav.getRoute("n", "s", "Fahrrad"), IOException );
+             assertEquals(testNav.gibRoute("n", "s", "Fahrrad"), IOException );
             //coordinates are not on the land -> "Route not found"
-            //assertEquals(testNav.getRoute("Hamburg", "Köln", "Auto"), "[RouteLeg{distance=423875.4, duration=113074.2, summary=, steps=[], annotation=null}]");
+            assertEquals("the returned route is not equal the expected", testNav.gibRoute("Hamburg", "Köln", "Auto"),
+                         "[RouteLeg{distance=423875.4, duration=113074.2, summary=, steps=[], annotation=null}]");
 
      /*method 2: + gibDistanz(String origin, String destination, String profile): Int
 
             possibilities:
                 input is a not existing location -> IOException
+                    assertEquals(testNav.gibDistanz("n", "s", "Fahrrad"), IOException)
                 gibDistanz("Hamburg","Köln", "Fahrrad") -> 423875.4
+                    assertEquals("the returned distance is not equal the expected", testNav.gibDistanz("Hamburg", "Köln", "Auto"),
+                         "423875.4")
 
 
      Methode 3: gibZeit(String origin, String destination, String profile): String
 
             possibilities:
                 input is a not existing location -> IOException
+                    assertEquals(testNav.gibZeit("n", "s", "Fahrrad"), IOException)
                 gibZeit("Hamburg","Köln", "Fahrrad") -> 113074.2
+                    assertEquals("the returned distance is not equal the expected", testNav.gibZeit("Hamburg", "Köln", "Auto"),
+                         "113074.2")
 */
     }
-    /*
+    public void testEingabetransformator()throws IOException {
+
+        Eingabetransformator testEt = new Eingabetransformator();
+        /*
     class Eingabetransformator
-        method 1: transformierePunkt(): Point
+
+        method 1: transformPoint(String point): Point
             possibilities:
                 input is a not existing location -> IOException
+                    assertEquals(testEt.transformPoint("no Point"), IOException)
                 example 1: transformiereProfile("hamburg") -> [10.0, 53.55]
                 example 2: transformiereProfile("köln") -> [6.95778, 50.94222]
 
         method 2: transformiereProfile(): String
             possibilities:
-                input is not a point
-                example 1: transformierePunkt([10.0, 53.55]) -> "hamburg"
+                input is not a point -> IOException
+                example 1: transformPoint([10.0, 53.55]) -> "hamburg"
                 example 2: transformierePunkt([6.95778, 50.94222]) -> "köln"
      */
-
+    }
     /*
     class ProfileSwitcher
         method 1: switchProfile (String profile): void
