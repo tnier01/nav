fea0534ae131b2e89c0e512aa9ac1f331af39ca2
t_nier01 <t_nier01@uni-muenster.de>
2019-03-13 12:37:28 +0100

    Eingabefenster lauffähig

--- src/main/java/ProfileSwitcher.java
+++ src/main/java/ProfileSwitcher.java
@@ -2,29 +2,23 @@ import com.mapbox.api.directions.v5.DirectionsCriteria;
 
 public class ProfileSwitcher {
     /**
-     * method which switch between the three different profiles driving, driving-traffic and walking depending on input.
+     * method which switch between the three different profiles "driving", "driving-traffic", "walking", "cycling" depending on input.
      * @param profile
      * @return
      */
     public String switchProfile(String profile) {
         String finalprofile=null;
         switch (profile) {
-            case "car":
-            case "Auto":
+            case "driving":
                 finalprofile = DirectionsCriteria.PROFILE_DRIVING;
                 break;
-            case "traffic":
-            case "Stau":
+            case "driving-traffic":
                 finalprofile = DirectionsCriteria.PROFILE_DRIVING_TRAFFIC;
                 break;
-            case "Laufen":
-            case "Gehen":
             case "walking":
                 finalprofile = DirectionsCriteria.PROFILE_WALKING;
                 break;
             case "cycling":
-            case "Fahrrad fahren":
-            case "Fahrrad":
                 finalprofile = DirectionsCriteria.PROFILE_CYCLING;
                 break;
             default:  throw new IllegalArgumentException("no legal profile");
--- src/main/java/Prototyp.java
+++ src/main/java/Prototyp.java
@@ -1,4 +1,6 @@
+import com.mapbox.geojson.Point;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Prototyp {
 
@@ -7,8 +9,40 @@ public class Prototyp {
         Routenfinder navigation =new Routenfinder();
 
 
-    navigation.gibRoute("51.96,7.59","51.83,8.32", "Fahrrad");
+        Point point2 = Point
+                .fromLngLat(41.362023, 2.168638);
+
+        /*
+        input
+         */
+
+        // Create a new object for the input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("From where would you like to start your route?");
+        // Read the first input in the string "input1"
+        String input1 = sc.nextLine();
+        System.out.println("origin input: " + input1);
+
+        System.out.println("Where do you would like to end the route?");
+        // Read the second input in the string "input2"
+        String input2 = sc.nextLine();
+        System.out.println("destination input: " + input2);
+
+        System.out.println("which profile are you using (selection: driving, driving-traffic, walking, cycling)?");
+        // Read the third input in the string "input3"
+        String input3 = sc.nextLine();
+        System.out.println("profile input: " + input3);
+
+        System.out.println("route calculation:");
+
+        //Close the scanner (not necessary, but recommended)
+        sc.close();
+
+        // route calculation
+        navigation.gibRoute(input1, input2, input3);
+
+
+
     }
-
-
 }
