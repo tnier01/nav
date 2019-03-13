294e11858b0874b033f050274ace5d1be1c9ce14
t_nier01 <t_nier01@uni-muenster.de>
2019-03-13 14:52:47 +0100

    Eingabefenster lauffähig 3 (mit Ausgabe der exceptions)

--- src/main/java/Prototyp.java
+++ src/main/java/Prototyp.java
@@ -11,33 +11,47 @@ public class Prototyp {
 
         /*
         input
+        If there is no exception the route is returned
          */
 
-        // Create a new object for the input
-        Scanner sc = new Scanner(System.in);
+        while ( true )
+        {
+            try
+            {
+                // Create a new object for the input
+                Scanner sc = new Scanner(System.in);
 
-        System.out.println("From where would you like to start your route?");
-        // Read the first input in the string "input1"
-        String input1 = sc.nextLine();
-        System.out.println("origin input: " + input1);
+                System.out.println("From where would you like to start your route?");
+                // Read the first input in the string "input1"
+                String input1 = sc.nextLine();
+                System.out.println("origin input: " + input1);
 
-        System.out.println("Where do you would like to end the route?");
-        // Read the second input in the string "input2"
-        String input2 = sc.nextLine();
-        System.out.println("destination input: " + input2);
+                System.out.println("Where do you would like to end the route?");
+                // Read the second input in the string "input2"
+                String input2 = sc.nextLine();
+                System.out.println("destination input: " + input2);
 
-        System.out.println("which profile are you using (selection: driving, driving-traffic, walking, cycling)?");
-        // Read the third input in the string "input3"
-        String input3 = sc.nextLine();
-        System.out.println("profile input: " + input3);
+                System.out.println("which profile are you using (selection: driving, driving-traffic, walking, cycling)?");
+                // Read the third input in the string "input3"
+                String input3 = sc.nextLine();
+                System.out.println("profile input: " + input3);
 
-        System.out.println("route calculation:");
+                System.out.println("route calculation:");
 
-        //Close the scanner (not necessary, but recommended)
-        sc.close();
+                // route calculation
+                navigation.gibRoute(input1, input2, input3);
 
-        // route calculation
-        navigation.gibRoute(input1, input2, input3);
+                break;
+            }
+            catch ( Exception e )
+            {
+                System.err.println(e.getMessage()); // returns the message concerning the actual exception
+
+            }
+        }
+        System.exit( 0 );
+
+
 
 
 
