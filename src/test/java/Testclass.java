0600c468e3d673acdc469a2b5950bddb7dc2ecb6
nick1 <n_jaku01@wwu.de>
2019-03-18 14:29:30 +0100

    Testklasse Phil

--- src/test/java/Testclass.java
+++ src/test/java/Testclass.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-public class Test {
+public class Testclass {
 
-
+    @org.junit.Test
     public void testRoutenfinder() throws IOException {
 
         Routenfinder testNav = new Routenfinder();
@@ -38,7 +38,7 @@ public class Test {
                          "113074.2")
 */
     }
-
+    @org.junit.Test
     public void testEingabetransformator() throws IOException {
 
         Eingabetransformator testEt = new Eingabetransformator();
@@ -46,38 +46,38 @@ public class Test {
     class Eingabetransformator
 
         method 1: transformPoint(String point): Point
-            possibilities:
-                input is a not a point or a existing location -> IOException
-                    assertEquals(testEt.transformPoint("no Point"), IOException)
-                example 1: transformPoint("hamburg") -> [10.0, 53.55]
-                    assertEquals(testEt.transformPoint("hamburg"), [10.0, 53.55])
-                example 2: transformPoint("köln") -> [6.95778, 50.94222]
-                    assertEquals(testEt.transformPoint("köln"), [6.95778, 50.94222])
-                example 3: transformPoint("10.0, 53.55") -> [10.0, 53.55]
-                    assertEquals(testEt.transformPoint("10.0, 53.55"), [10.0, 53.55])
-                example 4: transformPoint("6.95778, 50.94222") -> [6.95778, 50.94222]
-                    assertEquals(testEt.transformPoint("6.95778, 50.94222"), [6.95778, 50.94222])
-
+            possibilities:*/
+        // input is a not a point or a existing location -> IOException
+        //assertEquals(testEt.transformPoint("no Point"), IOException)
+        // example 1: transformPoint("hamburg") -> [10.0, 53.55]
+        assertEquals(testEt.transformPoint("hamburg"), "(10.0, 53.55)");
+        // example 2: transformPoint("köln") -> [6.95778, 50.94222]
+        assertEquals(testEt.transformPoint("köln"), "(6.95778, 50.94222)");
+        // example 3: transformPoint("10.0, 53.55") -> [10.0, 53.55]
+        assertEquals(testEt.transformPoint("10.0, 53.55"), "(10.0, 53.55)");
+        // example 4: transformPoint("6.95778, 50.94222") -> [6.95778, 50.94222]
+        assertEquals(testEt.transformPoint("6.95778, 50.94222"), "(6.95778, 50.94222)");
+/*
         method 2: transformiereProfile(): String
             possibilities: only accesses the ProfileSwitcher -> therefore no tests necessary
 
 
      */
     }
-
-    public void testProfileSwitcher() throws IOException {
+    @org.junit.Test
+    public void testProfileSwitcher() {
 
         ProfileSwitcher testPS = new ProfileSwitcher();
     /*
     class ProfileSwitcher
         method 1: switchProfile (String profile): void
             possibilities:*/
-                //switchProfile(" ") -> IllegalArgumentException("no legal profile")
-                    //assertEquals(testPS.switchProfile(" "),IllegalArgumentException)
-               // switchProfile("car") -> "DirectionsCriteria.PROFILE_DRIVING"
-                    assertEquals(testPS.switchProfile("car"),"DirectionsCriteria.PROFILE_DRIVING");
-                //switchProfile("Auto") -> "DirectionsCriteria.PROFILE_DRIVING"
-                    assertEquals(testPS.switchProfile("Auto"),"DirectionsCriteria.PROFILE_DRIVING");
+        //switchProfile(" ") -> IllegalArgumentException("no legal profile")
+        //assertEquals(testPS.switchProfile(" "),IllegalArgumentException)
+        // switchProfile("car") -> "DirectionsCriteria.PROFILE_DRIVING"
+        assertEquals(testPS.switchProfile("driving"),"DirectionsCriteria.PROFILE_DRIVING");
+        //switchProfile("Auto") -> "DirectionsCriteria.PROFILE_DRIVING"
+                    /*assertEquals(testPS.switchProfile("Auto"),"DirectionsCriteria.PROFILE_DRIVING");
                 //switchProfile("traffic") -> "DirectionsCriteria.PROFILE_DRIVING_TRAFFIC"
                     assertEquals(testPS.switchProfile("traffic"),"DirectionsCriteria.PROFILE_TRAFFIC");
                 //switchProfile("Stau") -> "DirectionsCriteria.PROFILE_DRIVING_TRAFFIC"
@@ -93,7 +93,7 @@ public class Test {
                 //switchProfile("Fahrrad fahren") -> "DirectionsCriteria.PROFILE_CYCLING"
                     assertEquals(testPS.switchProfile("Fahrrad fahren"),"DirectionsCriteria.PROFILE_CYCLING");
                 //switchProfile("Fahrrad") -> "DirectionsCriteria.PROFILE_CYCLING"
-                    assertEquals(testPS.switchProfile("Fahrrad"),"DirectionsCriteria.PROFILE_CYCLING");
+                    assertEquals(testPS.switchProfile("Fahrrad"),"DirectionsCriteria.PROFILE_CYCLING");*/
 
     }
 
@@ -107,22 +107,21 @@ public class Test {
              input is a not existing location -> IOException */
         //assertEquals(testS.gibRoute("n", "s", "Fahrrad"), IOException );
         //coordinates are not on the land -> "Route not found"
-        //assertEquals("the returned route is not equal the expected", testS.gibRoute("Hamburg", "Köln", "Auto"),
+        //assertEquals("the returned route is not equal the expected", testS.getRoute((10.0, 53.55), (6.95778, 50.94222), "Auto"),
         //             "[RouteLeg{distance=423875.4, duration=113074.2, summary=, steps=[], annotation=null}]");
 
-        /*method 2: geocoder(String Eingabe): Point
-            input is a not a point or a existing location -> IOException
-                    assertEquals(testEt.transformPoint("no Point"), IOException)
-                example 1: geocoder("hamburg") -> [10.0, 53.55]
-                    assertEquals(testS.geocoder("hamburg"), [10.0, 53.55])
-                example 2: geocoder("köln") -> [6.95778, 50.94222]
-                    assertEquals(testS.geocoder("köln"), [6.95778, 50.94222])
-                example 3: geocoder("10.0, 53.55") -> [10.0, 53.55]
-                    assertEquals(testS.geocoder("10.0, 53.55"), [10.0, 53.55])
-                example 4: geocoder("6.95778, 50.94222") -> [6.95778, 50.94222]
-                    assertEquals(testS.geocoder("6.95778, 50.94222"), [6.95778, 50.94222])
-         */
+       //method 2: geocoder(String Eingabe): Point
+                //input is a not a point or a existing location -> IOException
+                //assertEquals(testEt.transformPoint("no Point"), IOException)
+                //example 1: geocoder("hamburg") -> [10.0, 53.55]
+                assertEquals(testS.geocoding("hamburg"), "(10.0, 53.55)");
+        //example 2: geocoder("köln") -> [6.95778, 50.94222]
+        assertEquals(testS.geocoding("köln"), "(6.95778, 50.94222)");
+        //example 3: geocoder("10.0, 53.55") -> [10.0, 53.55]
+        assertEquals(testS.geocoding("10.0, 53.55"), "(10.0, 53.55)");
+        //example 4: geocoder("6.95778, 50.94222") -> [6.95778, 50.94222]
+        assertEquals(testS.geocoding("6.95778, 50.94222"), "(6.95778, 50.94222)");
+
     }
 }
-
 
