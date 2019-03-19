c6da6c64cef168fa676935bd22335ae4666f275f
nick1 <n_jaku01@wwu.de>
2019-03-19 16:22:59 +0100

    Ausgabe extern

--- src/main/java/Ausgabe.java
+++ src/main/java/Ausgabe.java
@@ -1,10 +1,6 @@
 import com.mapbox.api.directions.v5.models.DirectionsRoute;
-import com.mapbox.api.geocoding.v5.models.CarmenFeature;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.lang.String;
 
 //import org.graalvm.compiler.loop.InductionVariable;
 
@@ -12,42 +8,17 @@ public class Ausgabe {
 
     /**
      * gives some navigation instructions for the route on the run console
-     *
-     * @param args
+
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException {
-        Routenfinder navigation = new Routenfinder();
-        Schnitstelle geocode = new Schnitstelle();
+    public void output(String origin, String destination, DirectionsRoute naviList) throws IOException {
 
+        System.out.println("Distance of the Route: " + (double) Math.round(naviList.distance()/10)/100 + " kilometers");
+        System.out.println("Duration of the Route: " + Math.round(naviList.duration()/3600) + " Hours " + Math.round(naviList.duration()%3600/60) + " Minutes");
+        // print in which direction the user should drive on the first street
+        System.out.println("navigation from " + origin + " to " + destination + ": \n" + naviList.legs().get(0).steps().get(0).maneuver().instruction());
 
-/*
-        DirectionsRoute navi = navigation.gibRoute("raesfeld", "münster", "car");
-
-        for(int i = 0; i < navi.legs().get(0).steps().size()-1; i++) {
-            int instructionSize = navi.legs().get(0).steps().get(i).voiceInstructions().size();
-            double distance = navi.legs().get(0).steps().get(i).distance();
-            if (distance < 1000){
-                System.out.println("stay on the street for "+ Math.round(distance/10)*10 + " meters");
-            } else {
-                System.out.println("stay on the street for "+ Math.round(distance/100)/10 + " kilometers");
-            }
-
-            System.out.println(navi.legs().get(0).steps().get(i).voiceInstructions().get(instructionSize-1).announcement());
-        }
-
-
-
-
-        List<String> waypoints = new ArrayList<>();
-        waypoints.add("Raesfeld");
-        //waypoints.add("Borken");
-        waypoints.add("Reken");
-        //waypoints.add("Coesfeld");
-        waypoints.add("Münster");
-
-        DirectionsRoute naviList = navigation.gibListRoute(waypoints, "driving");
-
+        // print how long the user should stay on the actual street and which street should be used next
         for (int j = 0; j < naviList.legs().size(); j++) {
 
             for (int i = 0; i < naviList.legs().get(j).steps().size() - 1; i++) {
@@ -56,42 +27,14 @@ public class Ausgabe {
                 if (distance < 1000) {
                     System.out.println("stay on the street for " + Math.round(distance / 10) * 10 + " meters");
                 } else {
-                    System.out.println("stay on the street for " + Math.round(distance / 100) / 10 + " kilometers");
+                    System.out.println("stay on the street for " + (double) Math.round(distance / 100) / 10 + " kilometers");
                 }
 
                 System.out.println(naviList.legs().get(j).steps().get(i).voiceInstructions().get(instructionSize - 1).announcement());
             }
 
+
+
         }
-*/
-        List<String> waypoints = new ArrayList<>();
-        waypoints.add("Raesfeld");
-        waypoints.add("Münster");
-
-        DirectionsRoute naviList = navigation.getListRoute(waypoints, "driving");
-
-        DirectionsRoute newRoute = navigation.goneAstray(naviList, "Weseler Straße (B 70)");
-
-        //System.out.println(newRoute);
-
-        List<CarmenFeature> pointInfo = geocode.geocodeToObj("Weseler Straße (B 70) Raesfeld");
-        // the streetname of the point
-        String street = pointInfo.get(0).placeName();
-        //System.out.println(pointInfo);
-        //System.out.println(naviList);
-        String string1 = "Weseler Straße";
-        String string2 = "abcdefg Straße B(70)";
-        int compare = string1.compareTo(string2);
-        System.out.println(compare);
-
-        Offroute onroute = new Offroute();
-        boolean ontheroute = onroute.stillOnRoute(naviList, "Weseler Straße (B 70) Raesfeld");
-        System.out.println(ontheroute);
-
-
-/*
-        Point point = geocode.geocoding("paschenvenne raesfeld");
-        System.out.println(point.toString());
-*/
     }
 }
--- src/main/java/Prototyp.java
+++ src/main/java/Prototyp.java
@@ -159,29 +159,8 @@ public class Prototyp {
             }
         }
 
-        System.out.println("Distance of the Route: " + (double) Math.round(naviList.distance()/10)/100 + " kilometers");
-        System.out.println("Duration of the Route: " + Math.round(naviList.duration()/3600) + " Hours " + Math.round(naviList.duration()%3600/60) + " Minutes");
-        // print in which direction the user should drive on the first street
-        System.out.println("navigation from " + input1 + " to " + input2 + ": \n" + naviList.legs().get(0).steps().get(0).maneuver().instruction());
-
-        // print how long the user should stay on the actual street and which street should be used next
-        for (int j = 0; j < naviList.legs().size(); j++) {
-
-            for (int i = 0; i < naviList.legs().get(j).steps().size() - 1; i++) {
-                int instructionSize = naviList.legs().get(j).steps().get(i).voiceInstructions().size();
-                double distance = naviList.legs().get(j).steps().get(i).distance();
-                if (distance < 1000) {
-                    System.out.println("stay on the street for " + Math.round(distance / 10) * 10 + " meters");
-                } else {
-                    System.out.println("stay on the street for " + (double) Math.round(distance / 100) / 10 + " kilometers");
-                }
-
-                System.out.println(naviList.legs().get(j).steps().get(i).voiceInstructions().get(instructionSize - 1).announcement());
-            }
-
-
-
-        }
+       Ausgabe output = new Ausgabe();
+        output.output(input1, input2, naviList);
         //
         Map map = new Map();
         map.showMap();
